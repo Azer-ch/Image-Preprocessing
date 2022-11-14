c68fc35bd67242330d04d90d37fff05d4aa6b549
azer <azerchabbar@gmail.com>
2022-11-14 11:50:13 +0100

    tp2

--- tp/main.cpp
+++ tp/main.cpp
@@ -190,38 +190,107 @@ filter(pair<int, int> x, pair<int, int> y, vector<vector<string>> &image, int lx
     return ret;
 }
 
+void addBruit(vector<vector<string>> &image, int lx, int ly) {
+    int randomNumber;
+    for (int i = 0; i < lx; i++) {
+        for (int j = 0; j < ly; j++) {
+            randomNumber = rand() % 21;
+            if (randomNumber == 0)
+                image[i][j] = "0";
+            else if (randomNumber == 20)
+                image[i][j] = "255";
+        }
+    }
+}
+
+vector<vector<string>> aggregateFilter(vector<vector<string>> image, int lx, int ly, int n) {
+    vector<vector<string>> ret(lx, vector<string>(ly));
+    for (int i = 0; i < lx; i++) {
+        for (int j = 0; j < ly; j++) {
+            if (i - n / 2 >= 0 && i + n / 2 < lx && j - n / 2 >= 0 && j + n / 2 < ly) {
+                double m = 0;
+                for (int k = i - n / 2; k <= i + n / 2; k++) {
+                    for (int z = j - n / 2; z <= j + n / 2; z++) {
+                        m += stoi(image[k][z]);
+                    }
+                }
+                m /= (n * n);
+                ret[i][j] = to_string((int) m);
+            } else
+                ret[i][j] = image[i][j];
+        }
+    }
+    return ret;
+}
+
+vector<vector<string>> medianFilter(vector<vector<string>> image, int lx, int ly, int n) {
+    vector<vector<string>> ret(lx, vector<string>(ly));
+    for (int i = 0; i < lx; i++) {
+        for (int j = 0; j < ly; j++) {
+            if (i - n / 2 >= 0 && i + n / 2 < lx && j - n / 2 >= 0 && j + n / 2 < ly) {
+                vector<int> pxs = {};
+                for (int k = i - n / 2; k <= i + n / 2; k++) {
+                    for (int z = j - n / 2; z <= j + n / 2; z++) {
+                        pxs.push_back(stoi(image[k][z]));
+                    }
+                }
+                std::sort(pxs.begin(), pxs.end());
+                ret[i][j] = to_string(pxs[(n * n) / 2]);
+            } else
+                ret[i][j] = image[i][j];
+        }
+    }
+    return ret;
+}
+vector<vector<string>>
+
 int main() {
     int lx, ly;
     Image image;
     vector<vector<string>> v = readImage("chat.pgm", lx, ly, image);
-    writeImagePgm("matrice", v, lx, ly, image);
-    pair<double, double> p = calcMeanEcartType(v, lx, ly);
-    cout << "average" << " ecart type" << endl;
-    cout << p.first << " " << p.second << endl;
-    vector<int> h = histogram(v, lx, ly, image.maxValue);
-    vector<int> hc = cumulatedHistogram(h, image.maxValue);
-    cout << "Histogram: " << endl;
-    for (int x: h)
-        cout << x << " ";
-    cout << endl;
-    cout << "Cumulated Histogram: " << endl;
-    for (int x: hc)
-        cout << x << " ";
-    cout << endl;
-    vector<vector<string>> eq = equalizeImage(v, lx, ly, image.maxValue);
-    writeImagePgm("equalized", eq, lx, ly, image);
-    vector<int> n = normalization(hc, image.maxValue, lx, ly);
-    vector<int> eqHistogram = equalizedHistogram(n, h, image.maxValue);
-    cout << "Normalization\n";
-    for (int x: n) {
-        cout << x << " ";
-    }
-    cout << endl;
-    cout << "Equalized Histogram\n";
-    for (int x: eqHistogram) {
-        cout << x << " ";
-    }
-    cout << endl;
-    vector<vector<string>> filteredImage = filter({50, 100}, {150, 200}, v, lx, ly, image.maxValue);
-    writeImagePgm("filter", filteredImage, lx, ly, image);
+//    writeImagePgm("matrice", v, lx, ly, image);
+//    pair<double, double> p = calcMeanEcartType(v, lx, ly);
+//    cout << "average" << " ecart type" << endl;
+//    cout << p.first << " " << p.second << endl;
+//    vector<int> h = histogram(v, lx, ly, image.maxValue);
+//    vector<int> hc = cumulatedHistogram(h, image.maxValue);
+//    cout << "Histogram: " << endl;
+//    for (int x: h)
+//        cout << x << " ";
+//    cout << endl;
+//    cout << "Cumulated Histogram: " << endl;
+//    for (int x: hc)
+//        cout << x << " ";
+//    cout << endl;
+//    vector<vector<string>> eq = equalizeImage(v, lx, ly, image.maxValue);
+//    writeImagePgm("equalized", eq, lx, ly, image);
+//    vector<int> n = normalization(hc, image.maxValue, lx, ly);
+//    vector<int> eqHistogram = equalizedHistogram(n, h, image.maxValue);
+//    cout << "Normalization\n";
+//    for (int x: n) {
+//        cout << x << " ";
+//    }
+//    cout << endl;
+//    cout << "Equalized Histogram\n";
+//    for (int x: eqHistogram) {
+//        cout << x << " ";
+//    }
+//    cout << endl;
+//    vector<vector<string>> filteredImage = filter({50, 100}, {150, 200}, v, lx, ly, image.maxValue);
+//    writeImagePgm("filter", filteredImage, lx, ly, image);
+    writeImagePgm("duplicate", v, lx, ly, image);
+    addBruit(v, lx, ly);
+    vector<vector<string>> agg = aggregateFilter(v, lx, ly, 3);
+    vector<vector<string>> median = medianFilter(v, lx, ly, 3);
+    writeImagePgm("median", median, lx, ly, image);
+    writeImagePgm("aggregate", agg, lx, ly, image);
+    vector<vector<string>> test {
+            {"1", "1", "1", "1", "1"},
+            {"1", "2", "3", "4", "1"},
+            {"1", "10", "12", "13", "1"},
+            {"1", "5", "6", "5", "1"},
+            {"1", "1", "1", "1", "1"}
+    };
+    writeImagePgm("test", test, 5, 5, image );
+    writeImagePgm("testmoy",aggregateFilter(test,5,5,3),5,5,image);
 }
